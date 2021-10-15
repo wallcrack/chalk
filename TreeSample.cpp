77455776e2bd9b9a3015a9e67f561ff67fdda942
wallcrack <92564987+wallcrack@users.noreply.github.com>
2021-10-15 21:25:22 +0800

    Add files via upload

--- TreeSample.cpp
+++ TreeSample.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main()
 {
-	TreeMaker tm(10,100,0);
-	tm.RandomNormal();
-	auto edge=tm.GetEdge();
+	TreeMaker tm(10,100);//(a1,a2,a3),a1表示树的大小，a2表示树的最大权值
+	tm.RandomNormal();//随机一棵树
+	auto edge=tm.GetEdge();//获得树边
 	for(int i=0;i<edge.size();i++)
 		cout<<edge[i].u<<" "<<edge[i].v<<" "<<edge[i].w<<endl;
 	return 0;
