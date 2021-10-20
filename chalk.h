98e58d1e4201ea6945c8b64656a41e6d0023fa95
wallcrack <92564987+wallcrack@users.noreply.github.com>
2021-10-20 15:46:22 +0800

    Add files via upload
    多了随机图的生成器，但是只适用于点较少的简单连通图

--- chalk.h
+++ chalk.h
@@ -1,4 +1,5 @@
 #include<random>
+#include<map>
 #include<algorithm>
 #include<vector>
 #include<ctime>
@@ -26,7 +27,8 @@ class TreeMaker
 			{
 				int ver=rand()%i+1,wei=rand()%maxWeight;
 				while(ver==i)ver=rand()%i+1;
-				edge.push_back(GraphEdge(ver,i,wei));
+				if(rand()&1)edge.push_back(GraphEdge(ver,i,wei));
+				else edge.push_back(GraphEdge(i,ver,wei));//50%¸ÅÂÊ½»»»
 			}
 			std::shuffle(edge.begin(),edge.end(),random_device());
 		}
@@ -74,4 +76,53 @@ class SequenceMaker
 	{
 		return seq;
 	}
+};
+class GraphMaker
+{
+	private:
+		int n,m,maxWeight;
+		vector<GraphEdge>edge;
+	public:
+		GraphMaker(int a1=10,int a2=20,long long a3=20)
+		{
+			n=a1,m=a2,maxWeight=a3;
+			TreeMaker tm;
+			tm.RandomNormal();
+			edge=tm.GetEdge();
+		}
+		void MakeGraphEdge(vector<GraphEdge> &tmp,map<pair<int,int>,bool> &repeat)
+		{
+			double k=0.5;//p
+			for(int u=1;u<=n;u++)
+			{
+				for(int v=1;v<=n;v++)
+				{
+					if(u==v or repeat[make_pair(u,v)])continue;
+					int val=rand()%10000;
+					if(val<10000*k)continue;
+					long long w=rand()%maxWeight;
+					tmp.push_back(GraphEdge(u,v,w));
+				}
+			}
+		}
+		void RandomNormal()
+		{
+			mt19937 rand(time(0));
+			map<pair<int,int>,bool>rep;
+			TreeMaker tm(n,maxWeight);
+			tm.RandomNormal();
+			edge=tm.GetEdge();
+			for(int i=0;i<edge.size();i++)
+				rep[make_pair(edge[i].u,edge[i].v)]=true;
+			vector<GraphEdge> tmp;
+			MakeGraphEdge(tmp,rep);
+			shuffle(tmp.begin(),tmp.end(),rand);
+			for(int i=0;i<=m-n;i++)
+				edge.push_back(tmp[i]);
+			shuffle(edge.begin(),edge.end(),rand);
+		}
+		vector<GraphEdge> GetEdge()
+		{
+			return edge;
+		}
 };
