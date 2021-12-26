b994d2afc38cb9a1b72f5f028c4c9270e822c798
wallcrack <92564987+wallcrack@users.noreply.github.com>
2021-12-26 18:27:04 +0800

    Add files via upload

--- chalk.h
+++ chalk.h
@@ -1,51 +1,10 @@
 #include<random>
+#include<queue>
 #include<map>
 #include<algorithm>
 #include<vector>
 #include<ctime>
 using namespace std;
-class GraphEdge
-{
-	public:
-	int u,v,w;
-	GraphEdge(int c1,int c2,int c3)
-	{
-		u=c1,v=c2,w=c3;
-	}
-};
-class TreeMaker
-{
-	private:
-		int n,maxWeight,type;
-	public:
-		vector<GraphEdge>edge;
-		void RandomNormal()
-		{
-			edge.clear();
-			mt19937 rand(time(0));
-			for(int i=2;i<=n;i++)
-			{
-				int ver=rand()%i+1,wei=rand()%maxWeight;
-				while(ver==i)ver=rand()%i+1;
-				if(rand()&1)edge.push_back(GraphEdge(ver,i,wei));
-				else edge.push_back(GraphEdge(i,ver,wei));//50%æ¦‚çŽ‡äº¤æ¢
-			}
-			std::shuffle(edge.begin(),edge.end(),random_device());
-		}
-		void Set(int c1=10,int c2=10,int c3=1)
-		{
-			n=c1,maxWeight=c2,type=c3;
-		}
-		vector<GraphEdge> GetEdge()
-		{
-			return edge;
-		}
-		TreeMaker(int c1=10,int c2=10,int c3=1)
-		{
-			n=c1,maxWeight=c2,type=c3;
-			RandomNormal();
-		}
-};
 class SequenceMaker
 {
 	private:
@@ -77,6 +36,71 @@ class SequenceMaker
 		return seq;
 	}
 };
+class GraphEdge
+{
+	public:
+	int u,v,w;
+	GraphEdge(int c1,int c2,int c3)
+	{
+		u=c1,v=c2,w=c3;
+	}
+};
+class TreeMaker
+{
+	private:
+		int n,maxWeight,type;
+	public:
+		vector<GraphEdge>edge;
+		void PruferConstruct()
+		{
+			mt19937 rand(time(0));
+			SequenceMaker sm(n,n);
+			auto prufer=sm.GetSequence();
+			vector<int> deg(n+10,1);
+			set<int> leaves;
+			for(int i:prufer)deg[i]++;
+			for(int i=1;i<=n;i++)
+				if(deg[i]==1)leaves.insert(i);
+			for(int v:prufer)
+			{
+				int leaf=*leaves.begin();
+				leaves.erase(leaves.begin());
+				edge.push_back(GraphEdge(leaf,v,rand()%maxWeight));
+				if(--deg[v]==1)leaves.insert(v);
+			}
+			edge.push_back(GraphEdge(*leaves.begin(),n,rand()%maxWeight));
+			for(auto e:edge)
+				if(rand()&1)swap(e.u,e.v);
+			std::shuffle(edge.begin(),edge.end(),random_device());
+		}
+		void ConstructNormally()
+		{
+			mt19937 rand(time(0));
+			edge.clear();
+			for(int i=2;i<=n;i++)
+			{
+				int ver=rand()%i+1,wei=rand()%maxWeight;
+				while(ver==i)ver=rand()%i+1;
+				if(rand()&1)edge.push_back(GraphEdge(ver,i,wei));
+				else edge.push_back(GraphEdge(i,ver,wei));//50%¸ÅÂÊ½»»»
+			}
+			std::shuffle(edge.begin(),edge.end(),random_device());
+		}
+		void Set(int c1=10,int c2=10,int c3=1)
+		{
+			n=c1,maxWeight=c2,type=c3;
+		}
+		vector<GraphEdge> GetEdge()
+		{
+			return edge;
+		}
+		TreeMaker(int c1=10,int c2=10,int c3=1)
+		{
+			n=c1,maxWeight=c2,type=c3;
+			ConstructNormally();
+		}
+};
+
 class GraphMaker
 {
 	private:
@@ -86,8 +110,7 @@ class GraphMaker
 		GraphMaker(int a1=10,int a2=20,long long a3=20)
 		{
 			n=a1,m=a2,maxWeight=a3;
-			TreeMaker tm;
-			tm.RandomNormal();
+			TreeMaker tm(n,maxWeight);
 			edge=tm.GetEdge();
 		}
 		void MakeGraphEdge(vector<GraphEdge> &tmp,map<pair<int,int>,bool> &repeat)
@@ -110,7 +133,7 @@ class GraphMaker
 			mt19937 rand(time(0));
 			map<pair<int,int>,bool>rep;
 			TreeMaker tm(n,maxWeight);
-			tm.RandomNormal();
+			tm.ConstructNormally();
 			edge=tm.GetEdge();
 			for(int i=0;i<edge.size();i++)
 				rep[make_pair(edge[i].u,edge[i].v)]=true;
